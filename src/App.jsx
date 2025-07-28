eba0e58a8e05fb6d8ae7abb9cfeab6d161951dcd
Timirzin Egor <timirzin.egor@ya.ru>
2025-07-28 14:14:23 +0300

    Добавлено отображение на карте фигур из файла

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
-import { useEffect } from "react";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
-import "./style.css";
+import { useEffect } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import './style.css';
 
 function App() {
   useEffect(() => {
-    const map = L.map("map-id", { zoomControl: false }).setView(
-      [51.505, -0.09],
-      13
-    );
+    const map = L.map('map-id', { zoomControl: false }).setView([50, 40], 3);
 
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    fetch('/shapes.json')
+      .then((response) => response.json())
+      .then((shapes) =>
+        shapes.forEach(
+          ({
+            properties: { id, name },
+            geometry: { coordinates, type: shapeType },
+          }) => {
+            let shape = null;
+            switch (shapeType) {
+              case 'Polygon':
+                shape = L.polygon(coordinates[0]).addTo(map);
+                break;
+              case 'Point':
+                shape = L.marker(coordinates).addTo(map);
+                break;
+              default:
+                console.error(`Неизвестный тип фигуры: ${shapeType}`);
+                break;
+            }
+          }
+        )
+      );
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
